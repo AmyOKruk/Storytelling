866c907abeda0099f33f6b4e5ab75582b74f9c7b
Amy O'Kruk <amy.okruk@gmail.com>
2019-10-29 22:09:51 -0400

    Updated

--- src/scripts/homework-13/02-pie-multiples.js
+++ src/scripts/homework-13/02-pie-multiples.js
@@ -85,5 +85,61 @@ function ready(datapoints) {
         .attr('font-size', 15)
         .attr('fill', 'black')
         .attr('text-anchor', 'middle')
-    })
+      })
+
+      function render() {
+        // Grabbing the div that our svg is inside of
+        // and asking it wide it is
+        // "hey <svg> that is really a <g>, go through
+        // your parents until you find a div"
+        const svgContainer = svg.node().closest('div')
+        const svgWidth = svgContainer.offsetWidth
+        console.log(svgWidth)
+    
+        // .node() means "no really give me the HTML element,
+        //    not the weird d3 representation"
+        // .parentNode means "give me the svg that's outside
+        //    of the g," which we can actually change
+        //    the size of with .attr
+        // .closest('svg') means "go through your parents until
+        //   you find an svg, in case we have a g in a g in a g"
+        const actualSvg = d3.select(svg.node().closest('svg'))
+        actualSvg.attr('width', svgWidth)
+    
+        // Remember how we do
+        //    var width = 700 - margin.left - margin.right?
+        // this is the same thing, since svgWidth is the FULL
+        // SIZE of the svg, not the drawing area (the g)
+        const newWidth = svgWidth - margin.left - margin.right
+    
+        // Update our axes
+        // First, update the scale
+        // Then, update the axis
+        xPositionScale.range([0, newWidth])
+    
+        // What's the right number of ticks?
+        if (svgWidth < 400) {
+          xAxis.ticks(2)
+        } else if (svgWidth < 550) {
+          xAxis.ticks(4) // only have 3 ticks
+        } else {
+          xAxis.ticks(null) // resets it to the default number of ticks
+        }
+    
+        svg.select('.x-axis').call(xAxis)
+    
+        svg
+          .selectAll('.country-circle')
+          .attr('cx', d => xPositionScale(d.gdp_per_capita))
+    
+        console.log('resized to 500')
+      }
+    
+      // When the window resizes, run the function
+      // that redraws everything
+      d3.select(window).on('resize', render)
+    
+      // And now that the page has loaded, let's just try
+      // to do it once before the page has resized
+      render()
 }
